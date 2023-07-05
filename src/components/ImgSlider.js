389c28cf77956958f5ba43dddf3475ba3e7f0e27
vboxuser <vboxuser@ubuntu1.myguest.virtualbox.org>
2023-07-05 23:02:07 +0530

    slick-list designing

--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -1,19 +1,63 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 import React from 'react'
-import Styled from "styled-components"
+import Styled, { styled } from "styled-components"
 import Slider from "react-slick"
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
 
 
 function ImgSlider() {
+
+
+  let settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slideToShow: 1,
+    slideToScroll: 1,
+    autoplay: true
+
+  };
   return (
-    <div>
+    <Carousel {...settings}> 
+<Wrap>
+
+<img src="/images/slider-badging.jpg"/>
+</Wrap>
+<Wrap>
+
+<img src="/images/slider-badag.jpg"/>
+</Wrap>
 
 
-      
-    </div>
+    </Carousel>
   )
-}
+
+
+
+
+  }
+
 
 export default ImgSlider
+
+const Carousel = styled(Slider)`
+margin-top: 10px;
+.slick-list{
+   overflow: visible;
+}
+
+`
+const Wrap = styled.div`
+img{
+  width: 100%;
+  height:100;
+  border-radius: 10px;
+  box-shadow: rgba(0 0 0 / 69%) 0px 26px 30px -10px,
+  rgba( 0 0 0 / 73%) 0px 16px 10px -10px;
+
+ 
+}
+`
